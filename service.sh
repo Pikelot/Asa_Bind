747b27a739d886a7fc2ca6163ad320ce394d1ba3
jao <silvrt3d@mail.com>
2024-11-18 17:58:32 -0300

    service.sh quase pronto, alguns retoques necessários, nada demais

--- service.sh
+++ service.sh
@@ -1,16 +1,111 @@
 #!/bin/bash
-
-echo "Atualizando o container com as imagens..."
+ids=()
 
 ##if docker image ls | grep -q 
 
-docker build -t ubuntu-bind  atividade-asa01/dns
-docker build -t nginx atividade-asa01/web
+if [[ $1 == "start" ]]; then
+    
+    echo "Atualizando o container com as imagens..."
+    docker build -t ubuntu-bind  atividade-asa01/dns
+    docker build -t nginx atividade-asa01/web
 
-echo "as imagens atuais são: "
-docker image ls
+    echo "rodando o container..."
+    docker run -d -p 53:53/udp -p 53:53/tcp --name bind9 ubuntu-bind
+    docker run -d -p 80:80/tcp --name web nginx
 
-echo "rodando o container..."
-docker run -d -p 53:53/udp -p 53:53/tcp --name bind9 ubuntu-bind
+    clear
 
-docker run -d -p 80:80/tcp --name web nginx
+    echo "as imagens atuais são: "
+    docker image ls
+    echo ""
+    echo "os containeres em execução são: "
+    docker ps -a
+    echo ""
+fi
+
+if [[ $1 == "stop" ]]; then
+    
+    ids=($(docker ps -a --format '{{.ID}}'))
+    
+    echo "iniciando Interrupção e remoção dos containeres!"
+
+    for id in "${ids[@]}"; do
+        
+        container_name=$(docker ps -a --filter "id=$id" --format "{{.Names}}")
+
+        if [[ $container_name != "CONTAINER" ]]; then
+
+            docker stop $id
+            docker rm $id
+
+        fi
+    
+    done
+    
+    ids=($(docker image ls --format '{{.ID}}'))
+
+    echo "Iniciando a remoção das imagens"
+   
+    for id in "${ids[@]}"; do
+        
+        if [[ $container_name != "IMAGE" ]]; then
+            
+            docker rmi $id
+
+        fi
+    done
+
+fi
+
+if [[ $1 == "restart" ]]; then
+    
+    ids=($(docker ps -a --format '{{.ID}}'))
+    
+    echo "iniciando Interrupção e remoção dos containeres!"
+
+    for id in "${ids[@]}"; do
+        
+        container_name=$(docker ps -a --filter "id=$id" --format "{{.Names}}")
+
+        if [[ $container_name != "CONTAINER" ]]; then
+
+            docker stop $id
+            docker rm $id
+
+        fi
+    
+    done
+    
+    ids=($(docker image ls --format '{{.ID}}'))
+
+    echo "Iniciando a remoção das imagens"
+   
+    for id in "${ids[@]}"; do
+        
+        if [[ $container_name != "IMAGE" ]]; then
+            
+            docker rmi $id
+
+        fi
+    done
+
+    ###################
+
+    echo "Atualizando o container com as imagens..."
+    docker build -t ubuntu-bind  atividade-asa01/dns
+    docker build -t nginx atividade-asa01/web
+
+    echo "rodando o container..."
+    docker run -d -p 53:53/udp -p 53:53/tcp --name bind9 ubuntu-bind
+    docker run -d -p 80:80/tcp --name web nginx
+
+    clear
+
+    echo "as imagens atuais são: "
+    docker image ls
+    echo ""
+    echo "os containeres em execução são: "
+    docker ps -a
+    echo ""
+
+fi
